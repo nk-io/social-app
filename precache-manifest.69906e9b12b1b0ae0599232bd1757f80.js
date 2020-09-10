2483b2cf69cbed4ed8f9b0bd990133d583cd0028
nk-io <heysemkorkmaz@gmail.com>
2020-09-11 00:03:51 +0200

    Updates

---
+++ precache-manifest.69906e9b12b1b0ae0599232bd1757f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6393c48c4b1eb6641be85929fa19522",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "0c851a069aaf4fb2b66e",
+    "url": "/social-app/static/css/main.31e1246d.chunk.css"
+  },
+  {
+    "revision": "cfad4331d785af52b365",
+    "url": "/social-app/static/js/2.9791d20b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/social-app/static/js/2.9791d20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c851a069aaf4fb2b66e",
+    "url": "/social-app/static/js/main.3f3f47ef.chunk.js"
+  },
+  {
+    "revision": "c88df5a271eb7e060b91",
+    "url": "/social-app/static/js/runtime-main.14610cd3.js"
+  }
+]);
